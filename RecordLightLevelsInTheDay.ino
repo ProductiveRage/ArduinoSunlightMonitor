a5944e3ae9773574212d7f5cc21296d5e6f01975
ProductiveRage <dangger36@gmail.com>
2020-08-31 17:29:30 +0100

    Don't bother recording at night to save more power
    This change means that recordings stop between 10pm and 8am, according to the RTC - which I set to UTC. This definition of "night time" works for me in the UK where we're either on UTC time (GMT) or +1 (BST) and it makes little difference to me if "night time" is 10pm-8am at some times in the year or 11pm-9am in other times. If you lived in a different time zone then you might want to adjust this logic to sleep during more appropriate hours. Alternatively, you could keep this logic but set the RTC to be your local time and then work out whether any daylight saving hours changes are required based upon the date when you perform any post-processing yourself (if you set the RTC to your local time and your location has daylight savings changes then the time on the clock will be "wrong" for six months out of the year, which is why I decided to set it to UTC - it will always be correct in terms of UTC but I'll need to work out if the recorded time needs to have an hour added to it in Summer Time based upon the recorded date).

--- RecordLightLevelsInTheDay.ino
+++ RecordLightLevelsInTheDay.ino
@@ -36,11 +36,24 @@ void setup() {
 }
 
 void loop() {
+  DateTime now = rtc.now();
+  if ((now.hour() >= 22) || (now.hour() < 8)) {
+    // For my purposes, I don't care about light levels between 10pm and 8am and so the board can
+    // go to sleep during that period to conserve the battery - even when DST pushes the hour forward
+    // in the UK, I still don't care about 23:00 and 09:00 because that isn't a timeframe that I'll use
+    // the garden for.
+    // - I originally only checked if the hour was 22, which works if the sketch is left running without
+    //   stopping or resetting but if it's plugged into the USB for an update at midnight then that
+    //   would restart things and it would log readings
+    // You might want to tweak or even remove this entirely if you live in a time zone that is not as
+    // close to UTC as I am in the UK (either +0h or +1h from UTC at all times)
+    sleepUntil8am();
+  }
+  
   // Character arrays need to be long enough to store the number of "real" characters plus the
   // null terminator
   char filename[13]; // yyyyMMdd.txt = 12 chars + 1 null
   char timestamp[9]; // 00:00:00     =  8 chars + 1 null
-  DateTime now = rtc.now();
   snprintf(filename, sizeof(filename), "%04u%02u%02u.txt", now.year(), now.month(), now.day());
   snprintf(timestamp, sizeof(timestamp), "%02u:%02u:%02u", now.hour(), now.minute(), now.second());
 
@@ -75,5 +88,38 @@ void loop() {
   // - Using this instead of "delay" should mean that the battery will power the device for longer
   for (int i = 0; i < 3; i++) {
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+}
+
+void sleepUntil8am() {
+  Serial.println("It's late at night, going to sleep until 8am..");
+  Serial.flush(); // Ensure we finish sending serial messages before going to sleep
+  
+  while (true) {
+    DateTime now = rtc.now();
+    if (now.hour() == 8) {
+      break;
+    }
+
+    // To save as much power as possible, leave longer breaks before checking the current time to
+    // see whether it's time to return - if it's not even 7am yet then we can safely wait an hour
+    // or so before checking with the RTC again. If it's after 7 but before 07:30 then a half hour
+    // wait will be safe - after that, we'll check approximately every minute to try to get close
+    // to realising that it's 8am without using too much power in doing so.
+    int loopCount;
+    if (now.hour() == 7) {
+      if (now.minute() > 30) {
+        loopCount = 8; // 8 x 8s = 64s ~= 1m
+      }
+      else {
+        loopCount = 225; // 225 x 8s = 1800s = 0.5h
+      }
+    }
+    else {
+      loopCount = 450; // 450 x 8s = 3600s = 1h
+    }
+    for (int i = 0; i < loopCount; i++) {
+      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    }
   }
 }
